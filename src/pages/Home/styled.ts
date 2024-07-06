b8fde5625fe50969df1bd1f4169ec768bfb9ec4b
yejin <yejinchoe24@gmail.com>
2024-07-07 02:38:13 +0900

    Fix : 마진, 패딩 조정 Feat : 대학/과 말줄임 기능 추가

--- src/pages/Home/styled.ts
+++ src/pages/Home/styled.ts
@@ -3,67 +3,85 @@ import Carousel from "react-material-ui-carousel";
 import styled from "styled-components";
 
 export const Wrapper = styled(Box)`
-    width: 100%;
-    /* min-height: 1600px; */
+  width: 100%;
+  /* min-height: 1600px; */
 `;
 
 export const CarouselDiv = styled(Carousel)`
-    padding: 1rem;
+  padding: 0rem 2rem;
 `;
 
 export const BannerPaper = styled(Paper)`
-    p{
-        font-weight: 800;
-        font-size: 1.5rem;
-        letter-spacing: 0rem;
-    }
-    p:nth-of-type(2){
-        color: #1564ff;
-    }
-    b{
-        line-height: 1.5rem;
-    }
+  p {
+    font-weight: 800;
+    font-size: 1.3rem;
+    letter-spacing: 0rem;
+  }
+  p:nth-of-type(2) {
+    color: #1564ff;
+  }
+  b {
+    line-height: 1rem;
+  }
+
+  .css-ic3g2h-MuiPaper-root {
+    padding-top: 0.9rem;
+  }
 `;
 
 export const SearchDiv = styled(Box)`
-    padding: 1rem;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    h1{
-        font-size: 1.2rem;
-    }
+  padding: 3rem 2rem 1rem 2rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  h1 {
+    font-size: 1.3rem;
+  }
 `;
 
 export const CardDiv = styled(Box)`
+  padding: 1rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  h1 {
+    position: relative;
+    width: 90%;
+    left: 1%;
     padding: 1rem;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    h1{
-        width: 90%;
-        font-size: 1rem;
-    }
+    font-size: 1.2rem;
+  }
 `;
 
 export const CardWrap = styled(Paper)`
-    width: 90%;
-    background-color: #f5f5f5;
-    display: flex;
-    align-items: center;
+  width: 90%;
+  background-color: #f5f5f5;
+  display: flex;
+  box-sizing: border-box;
+  padding: 2rem !important;
+  align-items: center;
+  margin-top: 1rem !important;
+
+  .css-kem3np-MuiAvatar-root {
+    font-size: 1.5rem;
+  }
 `;
 
 export const CardTextDiv = styled.div`
-    height: 4rem;
-    flex: 1;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-around;
-    span{
-        font-size: .9rem;
-        color: #aaa;
-    }
-    p{
-        font-size: .9rem;
-    }
+  height: 4rem;
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  span {
+    font-size: 0.9rem;
+    color: #aaa;
+  }
+  p {
+    font-size: 0.9rem;
+    width: 10rem;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    overflow: hidden;
+  }
 `;
