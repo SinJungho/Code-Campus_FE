d9a698c776ced43ab1e567fd67454db9c541e009
SinJungho <lovelyzonmymind@gmail.com>
2024-07-07 02:01:02 +0900

    feat:로그인 창 작업 중

--- src/pages/Sign/login.tsx
+++ src/pages/Sign/login.tsx
@@ -1,14 +1,65 @@
-import React from 'react';
+import React from "react";
 import * as S from "./styled";
 import { Link } from "react-router-dom";
-import { Box } from '@mui/material';
+import { Box, Button, InputAdornment, TextField } from "@mui/material";
+import PersonIcon from "@mui/icons-material/Person";
+import LockIcon from "@mui/icons-material/Lock";
 
 const Login: React.FC = () => {
   return (
-    <Box>
-
-    </Box>
+    <S.Wrapper>
+      <TextField
+        id="outlined-basic"
+        label=""
+        placeholder="아이디"
+        variant="outlined"
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <PersonIcon />
+            </InputAdornment>
+          ),
+        }}
+      ></TextField>
+      <TextField
+        id="outlined-basic"
+        label=""
+        placeholder="비밀번호"
+        variant="outlined"
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <LockIcon />
+            </InputAdornment>
+          ),
+        }}
+      />
+      <Button variant="contained">로그인</Button>
+      <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+        <Checkbox />
+        <FindIdOrPw />
+      </Box>
+    </S.Wrapper>
   );
 };
+
+function Checkbox() {
+  return (
+    <S.CheckLabel>
+      <S.CheckInput type="checkbox" />
+      <p>로그인 유지</p>
+    </S.CheckLabel>
+  );
+}
+
+function FindIdOrPw() {
+  return (
+    <Box sx={{ display: "flex", alignItems: "center" }}>
+      <S.FindId href="#">아이디 찾기</S.FindId>
+      <S.Line />
+      <S.FindPw href="#">비밀번호 찾기</S.FindPw>
+    </Box>
+  );
+}
 
 export default Login;
--- src/pages/Sign/styled.ts
+++ src/pages/Sign/styled.ts
@@ -1,1 +1,64 @@
 import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { Box, Checkbox } from "@mui/material";
+import CheckIcon from "@mui/icons-material/Check";
+
+export const Wrapper = styled(Box)`
+  display: flex;
+  flex-direction: column;
+  gap: 0.6rem;
+`;
+
+// checkbox
+const checkIconSvg = encodeURIComponent(`
+  <svg xmlns="http://www.w3.org/2000/svg" fill="white" height="20" viewBox="0 96 960 960" width="20">
+    <path d="M384 840l-240-240 56-56 184 184 392-392 56 56-448 448z"/>
+  </svg>
+`);
+
+export const CheckInput = styled.input`
+  appearance: none;
+  width: 1rem;
+  height: 1rem;
+  border: 1.5px solid gainsboro;
+  border-radius: 50%;
+  background-color: gray;
+  background-image: url("data:image/svg+xml,${checkIconSvg}");
+  background-repeat: no-repeat;
+  background-position: 50%;
+  opacity: 0.5;
+
+  &:checked {
+    border-color: transparent;
+    background-image: url("data:image/svg+xml,${checkIconSvg}");
+    background-position: 50%;
+    background-repeat: no-repeat;
+    background-color: skyblue;
+  }
+`;
+
+export const CheckLabel = styled.label`
+  display: flex;
+  align-items: center;
+  user-select: none;
+`;
+
+export const CheckText = styled.p`
+  margin-left: 1.5rem;
+`;
+
+export const FindId = styled.a`
+  font-size: 1rem;
+`;
+
+export const Line = styled.span`
+  display: inline-block;
+  background-color: black;
+  margin: 0.5rem;
+  height: 100%;
+  width: 2px;
+`;
+
+export const FindPw = styled.a`
+  font-size: 1rem;
+`;
