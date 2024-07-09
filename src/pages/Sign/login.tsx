949a1f88ee80d3593bef3d70c3ce081c50d68d08
ljs001022 <ljs48425707@gmail.com>
2024-07-09 02:35:41 +0000

    Mobile JH

--- src/pages/Sign/login.tsx
+++ src/pages/Sign/login.tsx
@@ -1,14 +1,261 @@
-import React from 'react';
+import React, { useState } from "react";
 import * as S from "./styled";
 import { Link } from "react-router-dom";
-import { Box } from '@mui/material';
+import { Box, Button, InputAdornment, Switch, Tab } from "@mui/material";
+// Icons
+import PersonIcon from "@mui/icons-material/Person";
+import LockIcon from "@mui/icons-material/Lock";
+import CloseIcon from "@mui/icons-material/Close";
+import Logo from "../../assets/img/SNSLogo";
+import TabContext from "@mui/lab/TabContext";
+import TabList from "@mui/lab/TabList";
+import TabPanel from "@mui/lab/TabPanel";
 
 const Login: React.FC = () => {
-  return (
-    <Box>
+  const [value, setValue] = React.useState("1");
 
-    </Box>
+  const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+    setValue(newValue);
+  };
+
+  return (
+    <S.Wrapper>
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+          borderBottom: 1,
+          borderColor: "divider",
+          padding: "0.3rem 24px",
+        }}
+      >
+        <S.LogInTitle>로그인</S.LogInTitle>
+        <Link to="/index">
+          <CloseIcon sx={{ fontSize: "28px" }} />
+        </Link>
+      </Box>
+      <TabContext value={value}>
+        <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+          <TabList onChange={handleChange} variant="fullWidth">
+            <Tab
+              sx={{ width: "100%", fontSize: "0.5rem" }}
+              label="멘티 회원"
+              value="1"
+            />
+            <Tab
+              sx={{ width: "100%", fontSize: "0.5rem" }}
+              label="멘토 회원"
+              value="2"
+            />
+          </TabList>
+        </Box>
+        <TabPanel value="1">
+          <MenteeLogin />
+        </TabPanel>
+        <TabPanel value="2">
+          <MentorLogin />
+        </TabPanel>
+      </TabContext>
+    </S.Wrapper>
   );
 };
+
+// 멘티 회원 로그인 폼
+function MenteeLogin() {
+  return (
+    <S.Wrapper>
+      {/* 아이디 입력창 */}
+      <S.TextInput
+        id="outlined-basic"
+        label=""
+        placeholder="아이디"
+        variant="outlined"
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <PersonIcon sx={{ fontSize: 30 }} />
+            </InputAdornment>
+          ),
+        }}
+      ></S.TextInput>
+      {/* 비밀 번호 입력창 */}
+      <S.TextInput
+        id="outlined-basic"
+        label=""
+        placeholder="비밀번호"
+        variant="outlined"
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <LockIcon sx={{ fontSize: 30 }} />
+            </InputAdornment>
+          ),
+        }}
+      />
+      {/* 로그인 버튼 */}
+      <Button
+        sx={{
+          borderRadius: "12px",
+          fontSize: "0.5rem",
+          fontWeight: "bold",
+          padding: "0.4rem 0rem",
+          background: "#156aff",
+        }}
+        variant="contained"
+      >
+        로그인
+      </Button>
+      {/* 로그인 유지 */}
+      <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+        <Checkbox />
+        <FindIdOrPw />
+      </Box>
+      {/* SNS 회원 가입 & 로그인 */}
+      <SNSLogInButton />
+      {/* 개인 회원 가입 */}
+      <S.TextSignUp to="/signup">멘티 회원가입</S.TextSignUp>
+    </S.Wrapper>
+  );
+}
+
+function MentorLogin() {
+  const [checked, setChecked] = useState(false);
+  const handleChange = () => {
+    setChecked((prev) => !prev);
+  };
+  return (
+    <S.Wrapper>
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "flex-end",
+        }}
+      >
+        <Switch
+          checked={checked}
+          onChange={handleChange}
+          inputProps={{ "aria-label": "controlled" }}
+        />
+        <S.SwitchText>졸업생 회원</S.SwitchText>
+      </Box>
+      {/* 아이디 입력창 */}
+      <S.TextInput
+        id="outlined-basic"
+        label=""
+        placeholder={
+          checked === true ? "졸업생 회원 아이디" : "멘토 회원 아이디"
+        }
+        variant="outlined"
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <PersonIcon sx={{ fontSize: 30 }} />
+            </InputAdornment>
+          ),
+        }}
+      ></S.TextInput>
+      {/* 비밀 번호 입력창 */}
+      <S.TextInput
+        id="outlined-basic"
+        label=""
+        placeholder="비밀번호"
+        variant="outlined"
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <LockIcon sx={{ fontSize: 30 }} />
+            </InputAdornment>
+          ),
+        }}
+      />
+      <Button
+        sx={{
+          borderRadius: "12px",
+          fontSize: "0.5rem",
+          fontWeight: "bold",
+          padding: "0.4rem 0rem",
+          background: "#156aff",
+        }}
+        variant="contained"
+      >
+        로그인
+      </Button>
+      {/* 아이디, 비밀번호 찾기 */}
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "flex-end",
+        }}
+      >
+        <FindIdOrPw />
+      </Box>
+      {/* 멘티 회원 가입 */}
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          marginTop: "36px",
+        }}
+      >
+        <S.MentorSignUpLink to="/signup">멘토 회원가입</S.MentorSignUpLink>
+        <S.Line />
+        <S.MentorSignUpLink to="/signup">졸업생 회원가입</S.MentorSignUpLink>
+      </Box>
+    </S.Wrapper>
+  );
+}
+
+function SNSLogInButton() {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between",
+        width: "12rem",
+        margin: "36px auto",
+      }}
+    >
+      <S.NaverButton>
+        <Logo.NaverLogo />
+      </S.NaverButton>
+      <S.KakaoButton>
+        <Logo.KakaoLogo />
+      </S.KakaoButton>
+      <S.XButton>
+        <Logo.XLogo />
+      </S.XButton>
+      <S.GoogleButton>
+        <Logo.GoogleLogo />
+      </S.GoogleButton>
+      <S.AppleButton>
+        <Logo.AppleLogo />
+      </S.AppleButton>
+    </Box>
+  );
+}
+
+function Checkbox() {
+  return (
+    <S.CheckLabel>
+      <S.CheckInput type="checkbox" />
+      <S.CheckSubText>로그인 유지</S.CheckSubText>
+    </S.CheckLabel>
+  );
+}
+
+function FindIdOrPw() {
+  return (
+    <Box sx={{ display: "flex", alignItems: "center" }}>
+      <S.FindId href="#">아이디 찾기</S.FindId>
+      <S.Line />
+      <S.FindPw href="#">비밀번호 찾기</S.FindPw>
+    </Box>
+  );
+}
 
 export default Login;
--- src/pages/Sign/styled.ts
+++ src/pages/Sign/styled.ts
@@ -1,1 +1,228 @@
 import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { Box, TextField, StepLabel, Stepper } from "@mui/material";
+import CheckIcon from "@mui/icons-material/Check";
+import { CheckBox } from "@mui/icons-material";
+
+export const Wrapper = styled(Box)`
+  display: flex;
+  flex-direction: column;
+  gap: 0.6rem;
+`;
+
+export const LogInTitle = styled.h4`
+  font-size: 0.55rem;
+`;
+
+// 아이디, 비밀번호 입력창
+export const TextInput = styled(TextField)`
+  & .MuiOutlinedInput-root {
+    border-radius: 14px;
+  }
+
+  & .MuiOutlinedInput-input {
+    padding: 0.5rem 0rem;
+    font-size: 0.5rem;
+    font-weight: bold;
+  }
+`;
+
+// checkbox
+const checkIconSvg = encodeURIComponent(`
+  <svg xmlns="http://www.w3.org/2000/svg" fill="white" height="20" viewBox="0 96 960 960" width="20">
+    <path d="M384 840l-240-240 56-56 184 184 392-392 56 56-448 448z"/>
+  </svg>
+`);
+
+export const CheckInput = styled.input`
+  appearance: none;
+  width: 1rem;
+  height: 1rem;
+  border: 1.5px solid gainsboro;
+  border-radius: 50%;
+  background-color: #a6a6a6;
+  background-image: url("data:image/svg+xml,${checkIconSvg}");
+  background-repeat: no-repeat;
+  background-position: 50%;
+
+  &:checked {
+    border-color: transparent;
+    background-image: url("data:image/svg+xml,${checkIconSvg}");
+    background-position: 50%;
+    background-repeat: no-repeat;
+    background-color: #156aff;
+  }
+`;
+
+export const CheckLabel = styled.label`
+  display: flex;
+  align-items: center;
+  user-select: none;
+`;
+
+export const CheckSubText = styled.p`
+  font-size: 0.5rem;
+  margin-left: 0.2rem;
+`;
+
+export const CheckText = styled.p`
+  margin-left: 1.5rem;
+`;
+
+export const FindId = styled.a`
+  font-size: 0.5rem;
+`;
+
+export const Line = styled.span`
+  display: inline-block;
+  background-color: gray;
+  margin: 0rem 0.3rem;
+  height: 0.6rem;
+  width: 1px;
+  opacity: 0.2;
+`;
+
+export const FindPw = styled.a`
+  font-size: 0.5rem;
+`;
+
+export const NaverButton = styled.button`
+  width: 40px;
+  height: 40px;
+  background: #11d166;
+  border-radius: 50%;
+  border: none;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+`;
+
+export const KakaoButton = styled.button`
+  width: 40px;
+  height: 40px;
+  background: #ffe812;
+  border-radius: 50%;
+  border: none;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const XButton = styled.button`
+  width: 40px;
+  height: 40px;
+  background: #222;
+  border-radius: 50%;
+  border: none;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const GoogleButton = styled.button`
+  width: 40px;
+  height: 40px;
+  background: #fff;
+  border-radius: 50%;
+  border: 0.5px solid #dadce0;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const AppleButton = styled.button`
+  width: 40px;
+  height: 40px;
+  background: #222;
+  border-radius: 50%;
+  border: none;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+// 개인 회원 가입
+export const TextSignUp = styled(Link)`
+  text-align: center;
+  font-size: 0.5rem;
+`;
+
+/**
+ * 졸업생, 멘토 회원
+ */
+
+// 졸업생 회원 스위치
+export const SwitchText = styled.p`
+  font-size: 0.5rem;
+`;
+
+export const MentorSignUpLink = styled(Link)`
+  font-size: 0.5rem;
+`;
+
+/*
+ * * 회원 가입
+ */
+
+export const SignUpLabel = styled.label`
+      display: flex;
+    align-items: center;
+    user-select: none;
+    background-color: #f9f9f9;
+    padding: 1.3rem 0.5rem;
+    margin-bottom: 1rem;
+    border-radius: 14px;
+}
+`;
+
+export const SignUpSubTitle = styled.span`
+  display: block;
+  font-size: 0.5rem;
+  opacity: 0.5;
+  font-weight: bold;
+  margin-bottom: 0.3rem;
+`;
+
+export const SignUpTitle = styled.p`
+  font-size: 1rem;
+  font-weight: bold;
+  margin-bottom: 1rem;
+`;
+
+export const ChooseMemberTypeTitle = styled.p`
+  font-size: 1rem;
+  font-weight: bold;
+`;
+
+export const ChooseMemberTypeContext = styled.span`
+  font-size: 0.5rem;
+`;
+
+/***
+ * Stepper
+ */
+
+export const StepperLabel = styled(StepLabel)`
+  && .MuiStepIcon-text {
+    font-size: 0.35rem;
+  }
+
+  && .MuiStepLabel-label {
+    font-size: 0.5rem;
+  }
+`;
+
+export const SignStepper = styled(Stepper)`
+  & .MuiStepConnector-line {
+    display: block;
+    border-color: #bdbdbd;
+    border-top-style: dotted;
+    border-top-width: 1.5px;
+  }
+`;
