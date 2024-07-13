96a991d65beca13a568a7d07ec07750f423fd652
SinJungho <lovelyzonmymind@gmail.com>
2024-07-13 23:55:27 +0900

    fix: Provider 파일 이동

--- src/pages/Tutor/TutorList.tsx
+++ src/pages/Tutor/TutorList.tsx
@@ -1,11 +1,21 @@
 import React from "react";
 import * as S from "./styled";
+import { Link } from "react-router-dom";
+import {
+  Box,
+  Avatar,
+  Typography,
+  Button,
+  Tab,
+  Tabs,
+  Badge,
+} from "@mui/material";
 
 const TutorList: React.FC = () => {
-    return(
-        <div>
-
-        </div>
-    );
+  return (
+    <Box>
+      <Typography>선배 탐색</Typography>
+    </Box>
+  );
 };
 export default TutorList;
