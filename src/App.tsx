e8655ca0d42446191471cd8b95c347926838c906
ljs001022 <ljs48425707@gmail.com>
2024-07-14 14:18:22 +0900

    feat: 스크롤 고정

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import GlobalStyles from './Globalstyles';
 import Router from './Router';
+import PageScrollTop from './component/PageScrollTop';
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
+      <PageScrollTop/>
       <GlobalStyles />
       <Router />
     </BrowserRouter>
